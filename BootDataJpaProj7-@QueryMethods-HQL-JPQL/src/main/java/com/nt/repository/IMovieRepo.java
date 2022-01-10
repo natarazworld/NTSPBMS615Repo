d67660153d261c235b9aedec15e18fc4046c0d80
Nareshit <Nareshit@DESKTOP-IUDAAVL>
2022-01-10 20:44:07 +0530

    spring data jpa   Query methods

--- BootDataJpaProj7-@QueryMethods-HQL-JPQL/src/main/java/com/nt/repository/IMovieRepo.java
+++ BootDataJpaProj7-@QueryMethods-HQL-JPQL/src/main/java/com/nt/repository/IMovieRepo.java
@@ -11,13 +11,33 @@ import com.nt.model.Movie;
 
 
 public interface IMovieRepo extends JpaRepository<Movie, Integer> {
-
+// =================== Entity queries (all col vlaues )=======================
 	 //@Query("from Movie where mid>=?1 and mid<=?2")
 	//@Query("from Movie as m where m.mid>=?1 and m.mid<=?2")
 	/*@Query("from Movie  where mid>=:min and mid<=:max")
 	 public List<Movie>  searchMoviesByIdRange(@Param("min") int start,@Param("max")  int end);*/
 	
-	@Query("from Movie  where mid>=:min and mid<=:max")
+	@Query("from Movie  where mid>=?1 and mid<=?2")  //Entity query
 	 public List<Movie>  searchMoviesByIdRange( int min, int max);
+	
+	@Query("from Movie  where  mname In(:name1,:name2,:name3) order by mname asc")
+	public  List<Movie>  searchMoviesByMnames(String  name1,String name2,String name3);   //taking  array /var args /collection params
+	                                                                                                                                                                        // multiple named params or postional params is not allowed
+	@Query("from Movie  where  mname In(?1,?2,?3) order by mname asc")
+	public  List<Movie>  searchMoviesByMnames1(String  name1,String name2,String name3 );
+	
+	//================= Scalar Queries (sepecific multiple col values) ================= 
+	 
+	@Query("select  mid,mname,year from Movie where rating>=:rat and year in(:y1,:y2,:y3) ")
+	public  List<Object[]>  fetchMovieDetailsByRatingsAndYear(float rat , String y1,String y2,String y3);
+	
+	
+	//================= Scalar Queries (sepecific single col values) ================= 
+	 
+		@Query("select mname  from Movie where year>=:start and year<=:end order by mname desc ")
+		public  List<String>  fetchMoviesByYearRange(String start,String end);
+		
+		
+		
 
 }
--- BootDataJpaProj7-@QueryMethods-HQL-JPQL/src/main/java/com/nt/runner/QueryMethodsTestRunner.java
+++ BootDataJpaProj7-@QueryMethods-HQL-JPQL/src/main/java/com/nt/runner/QueryMethodsTestRunner.java
@@ -1,12 +1,9 @@
 package com.nt.runner;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.nt.model.Movie;
 import com.nt.repository.IMovieRepo;
 
 @Component
@@ -17,8 +14,23 @@ public class QueryMethodsTestRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("====Query methods execution ============");
-		List<Movie> list=repo.searchMoviesByIdRange(200, 500);
-		list.forEach(System.out::println);
+		/*List<Movie> list=repo.searchMoviesByIdRange(200, 500);
+		list.forEach(System.out::println);*/
+		  repo.searchMoviesByMnames("RRR","Don","puspha").forEach(System.out::println);
+		  System.out.println("------");
+		  repo.searchMoviesByMnames1("RRR","Don","puspha").forEach(System.out::println);
+		  System.out.println("==================");
+		  repo.fetchMovieDetailsByRatingsAndYear(4.0f, "2020","2021","2022").forEach(row->{
+			      for(Object val:row)
+			    	  System.out.print(val+"  ");
+			      
+			      System.out.println();
+		  });
+		  
+		  System.out.println("==================");
+		  repo.fetchMoviesByYearRange("2020","2022").forEach(System.out::println);
+			
+		
 		
 		
 	}
